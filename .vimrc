1b0958688ef30ad9b9bde9d9ec9be7d578a12e2f
Kaan Karakaya <yusufkaan142@gmail.com>
2020-04-03 13:15:25 +0300

    Added git and ssh config
    Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

---
+++ .vimrc
@@ -0,0 +1,37 @@
+syntax on
+set number
+set ruler
+set colorcolumn=80
+set autoindent
+set smartindent
+
+" tabstop:          Width of tab character
+" softtabstop:      Fine tunes the amount of white space to be added
+" shiftwidth        Determines the amount of whitespace to add in normal mode
+" expandtab:        When on uses space instead of tabs
+set tabstop     =4
+set softtabstop =4
+set shiftwidth  =4
+set expandtab
+
+" show tabs
+set list
+set listchars=tab:\|_
+
+set ttyfast
+set mouse=a
+set ttymouse=xterm2
+
+set backspace=2 " make backspace work like most other apps
+set backspace=indent,eol,start
+
+" highlight unwanted spaces like trailing spaces, spaces before tab, tabs
+" that aren't at the start of a line.
+" (adopted from https://vim.fandom.com/wiki/Highlight_unwanted_spaces)
+highlight ExtraWhitespace ctermbg=red guibg=red
+match ExtraWhitespace /\s\+$\| \+\ze\t/
+match ExtraWhitespace /[^\t]\zs\t\+/
+autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+autocmd BufWinLeave * call clearmatches()
---
+++ git_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+EMAIL_1="yusufkaan142"
+EMAIL_2="gmail.com"
+
+set -x
+
+# author
+git config --global user.name "Kaan Karakaya"
+git config --global user.email "${EMAIL_1}@${EMAIL_2}"
+
+# use https remotes and osxkeychain for creds
+git config --global url.git\@github\.com\:.pushInsteadOf https://github.com/
+git config --global gpg.program "gpg"
+
+# use ssh in hub (commented out since pushInsteadOf)
+# git config --global hub.protocol ssh # https://github.com/github/hub/issues/1614
+
+# diff-so-fancy and its color scheme
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+git config --global color.diff-highlight.oldNormal "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+git config --global color.diff.meta "227"
+git config --global color.diff.frag "magenta bold"
+git config --global color.diff.commit "227 bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+# rebase helper
+git config --global sequence.editor interactive-rebase-tool
+
+# install symlink for ssh config
+SSH_CONFIG="$HOME/.ssh/config"
+if [[ -f "$SSH_CONFIG" ]]; then
+	rm "$SSH_CONFIG"
+fi
+ln -s "$SCRIPT_DIR/ssh_config" "$SSH_CONFIG"
+
+git config --global core.editor "code --wait"
+git config --global core.excludesfile ~/.gitignore_global
+
+# use vscode as the editor
+# git config --global core.editor "code --wait"
